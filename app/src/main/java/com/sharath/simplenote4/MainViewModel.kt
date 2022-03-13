7b26c7ca3d9d87056f3da84735c5e07964d6cc13
Sharath <mailtokgsharath@gmail.com>
2022-03-13 13:31:02 +0530

    Note4: Add a navigation controller to the activity

--- app/src/main/java/com/sharath/simplenote4/MainViewModel.kt
+++ app/src/main/java/com/sharath/simplenote4/MainViewModel.kt
@@ -1,7 +1,44 @@
 package com.sharath.simplenote4
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.sharath.simplenote4.data.AppDatabase
+import com.sharath.simplenote4.data.NoteEntity
+import com.sharath.simplenote4.data.SampleDataProvider
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(app: Application) : AndroidViewModel(app) {
+
+    private val database = AppDatabase.getInstance(app)
+    val notesList = database?.noteDao()?.getAll()
+
+    fun addSampleData() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                val sampleNotes = SampleDataProvider.getNotes()
+                database?.noteDao()?.insertAll(sampleNotes)
+            }
+        }
+    }
+
+    fun deleteNotes(selectedNotes: List<NoteEntity>) {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                database?.noteDao()?.deleteNotes(selectedNotes)
+            }
+        }
+
+    }
+
+    fun deleteAllNotes() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                database?.noteDao()?.deleteAll()
+            }
+        }
+    }
+
 }
